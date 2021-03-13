e6de4f4a49613bb6ea168f8b4c96120975768ea9
nguyễn thanh hiền <thanhhienm4@gmail.com>
2021-03-13 23:26:08 +0700

    Add-BackEndAPI project

---
+++ MovieTheater.BackEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovieTheater.BackEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
